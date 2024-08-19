2ad23255f287540acf7abf6415dba9411f538a8a
Gustavo Teixeira <gustavo.teixeira2199@gmail.com>
2024-08-19 20:11:19 -0300

    findAll pageable

--- src/main/java/com/tavo/client/controllers/ClientController.java
+++ src/main/java/com/tavo/client/controllers/ClientController.java
@@ -3,6 +3,8 @@ package com.tavo.client.controllers;
 import com.tavo.client.dto.ClientDTO;
 import com.tavo.client.services.ClientService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,5 +21,10 @@ public class ClientController {
     @GetMapping("/{id}")
     public ResponseEntity<ClientDTO> findById(@PathVariable Long id) {
         return ResponseEntity.ok(service.findById(id));
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<ClientDTO>> findAll(Pageable pageable) {
+        return ResponseEntity.ok(service.findAll(pageable));
     }
 }
--- src/main/java/com/tavo/client/services/ClientService.java
+++ src/main/java/com/tavo/client/services/ClientService.java
@@ -5,6 +5,8 @@ import com.tavo.client.entities.Client;
 import com.tavo.client.repositories.ClientRepository;
 import com.tavo.client.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -19,5 +21,11 @@ public class ClientService {
         Client client = repository.findById(id)
                 .orElseThrow(() -> new ResourceNotFoundException("Recurso não encontrado"));
         return new ClientDTO(client);
+    }
+
+    @Transactional(readOnly = true)
+    public Page<ClientDTO> findAll(Pageable pageable) {
+        Page<Client> result = repository.findAll(pageable);
+        return result.map(ClientDTO::new);
     }
 }
