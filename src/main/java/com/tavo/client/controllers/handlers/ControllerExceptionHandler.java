de64c1d0e7013f24c3cd4ba34898a496c6efa81d
Gustavo Teixeira <gustavo.teixeira2199@gmail.com>
2024-08-19 21:32:13 -0300

    FieldMessage, ValidationError

--- src/main/java/com/tavo/client/controllers/handlers/ControllerExceptionHandler.java
+++ src/main/java/com/tavo/client/controllers/handlers/ControllerExceptionHandler.java
@@ -1,10 +1,13 @@
 package com.tavo.client.controllers.handlers;
 
 import com.tavo.client.dto.CustomError;
+import com.tavo.client.dto.ValidationError;
 import com.tavo.client.services.exceptions.ResourceNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -17,6 +20,16 @@ public class ControllerExceptionHandler {
     public ResponseEntity<CustomError> resourceNotFound(ResourceNotFoundException e, HttpServletRequest request) {
         HttpStatus status = HttpStatus.NOT_FOUND;
         CustomError err = new CustomError(Instant.now(), status.value(), e.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(status).body(err);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<CustomError> methodArgumentNotValid(MethodArgumentNotValidException e, HttpServletRequest request) {
+        HttpStatus status = HttpStatus.UNPROCESSABLE_ENTITY;
+        ValidationError err = new ValidationError(Instant.now(), status.value(), "Dados inválidos", request.getRequestURI());
+        for(FieldError f : e.getBindingResult().getFieldErrors()) {
+            err.addError(f.getField(), f.getDefaultMessage());
+        }
         return ResponseEntity.status(status).body(err);
     }
 }
